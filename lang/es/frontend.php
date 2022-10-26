aa82593dd7fff1a7c98d43910e83cb50fd4749b7
Remo Gkardi <67518522+meynot@users.noreply.github.com>
2022-10-26 18:10:17 +0300

    frontend and navigation
    Navigation and frontend translation

---
+++ lang/es/frontend.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+	"navbar_home"=>"Home",
+	"navbar_abountus"=>"About us",
+	"navbar_sahaba_cards"=>"Sahaba cards",
+		"navbar_ten_promised_paradise"=>"Ten Promised Paradise",
+		"navbar_most_hadeeth_narrators"=>"Most hadeeth narrators",
+		"navbar_find_sahabi"=>"Find Sahabi",
+	
+	"navbar_sahaba_interducing"=>"Sahaba Info",
+	"navbar_contactus"=>"Contact us",
+
+];
