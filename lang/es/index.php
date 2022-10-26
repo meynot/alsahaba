13c56caeac3edc5044032f4d4bb60aa10f2a7e9c
Remo Gkardi <67518522+meynot@users.noreply.github.com>
2022-10-26 18:03:05 +0300

    more translation
    Added more translated words

--- lang/es/index.php
+++ lang/es/index.php
@@ -5,5 +5,12 @@ return [
 	"lang"=>"es",
 	"flag"=>"es",
 	"title"=>"Español",
-	
+    "action_create"=> "Crear",
+    "action_edit"=> "Editar",
+    "action_delete"=> "Eliminar",
+    "action_cancel"=> "Cancelar",
+    "acción_guardar"=> "Guardar",
+
+
+    "no_records_found"=> "No se encontraron datos",
 ];
