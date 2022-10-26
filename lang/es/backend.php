bf23da11980c9d9d99191d973aa61a3c76b5cd1c
Remo Gkardi <67518522+meynot@users.noreply.github.com>
2022-10-26 18:11:36 +0300

    backend navigation
    Dashboard and navigation translation file

---
+++ lang/es/backend.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	"signout"=>"Sign out",
+	"search"=>"Find",
+	"breadcrumbs_dashboard"=>"Dashboard",
+	
+	"sidebar_dashboard"=>"Dashboard",
+	"sidebar_posts_browse"=>"Browse posts",
+	"sidebar_users_browse"=> "Browse Users",
+	"sidebar_settings"=>"Settings",
+];
