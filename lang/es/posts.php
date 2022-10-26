f3a6ab508aecec65cd9ff70333c77e0e8a2672ae
Remo Gkardi <67518522+meynot@users.noreply.github.com>
2022-10-26 18:06:26 +0300

    posts translation
    The translation file of posts

---
+++ lang/es/posts.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  "header_browse"=>"Explorar publicaciones",
+  "header_create"=> "Crear nueva publicación",
+  "header_delete"=> "Eliminar publicación",
+  "warn_delete"=> "¿Está seguro de que desea eliminar esta publicación?"
+
+  "datafield_title"=>"Título",
+  "datafield_short"=>"Introducción",
+  "cuerpo_campo_datos"=>"Cuerpo",
+  "placeholder_body"=> "Por favor escriba el texto aquí<br />",
+  "datafield_is_published" => "Publicado",
+  "datafield_is_featured" => "Destacado",
+  "datafield_is_pinned" => "Fijado",
+  "datafield_is_commentable" => "Comentable",
+  "datafield_has_roles" => "Funciones",
+  "datafield_is_editable" => "Editable",
+  "datafield_is_deleteable" => "Eliminable",
+];
