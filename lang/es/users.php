46b471bde0378d382d0d802c10b3766a2c90dd60
Remo Gkardi <67518522+meynot@users.noreply.github.com>
2022-10-26 18:08:41 +0300

    users translation file
    Translation for users model

---
+++ lang/es/users.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+  "header_browse"=>"Buscar usuarios",
+  "header_create"=>"Crear usuario",
+  "header_delete"=>"Eliminar usuario",
+
+
+
+  "nombre_campo_datos"=> "Nombre",
+
+  "datafield_password_confirmation"=>"Confirmar contraseña",
+  "datafield_password"=>"Contraseña",
+
+  "datafield_email"=>"Correo electrónico",
+  "datafield_is_active"=> "Activo",
+  "datafield_roles"=> "Funciones",
+
+  "datafield_facebook"=> "Cuenta de Facebook",
+  "datafield_twitter"=>"Cuenta de Twitter",
+  "datafield_instagram"=>"Instagram",
+
+  "warn_delete"=>"¿Estás seguro de eliminar esto?",
+
+]
